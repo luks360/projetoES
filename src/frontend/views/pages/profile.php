70741dc056e314f4bb7053ad891ac02f04759c41
Lucas dos Santos <santoslucasantos796@gmail.com>
2021-09-27 21:00:59 -0300

    Correção de layout

--- src/frontend/views/pages/profile.php
+++ src/frontend/views/pages/profile.php
@@ -32,6 +32,7 @@ if(!$login_button == ''){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="../../../backend/custom.js"></script>
+    <script src="../../../backend/control.js"></script>
     
 </head>
 
